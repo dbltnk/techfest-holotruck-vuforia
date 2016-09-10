31bc884e1ed35399a3e52649782cc7543cbc2bce
Alexander Zacherl <alexander.zacherl@googlemail.com>
2016-09-10 15:22:59 +0200

    recognized marker

---
+++ Assets/debug.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class debug : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log(Application.loadedLevelName);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+}
